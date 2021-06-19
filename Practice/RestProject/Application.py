fac47ce6170a29815320dc2d3a5d270d51392bbe
Ashok Kumar Jha <ashu.kr.jha@gmail.com>
2021-06-20 00:47:04 +0530

    Modified DB  Part

--- Practice/RestProject/Application.py
+++ Practice/RestProject/Application.py
@@ -4,14 +4,23 @@ Created on Sat Jun 19 15:05:44 2021
 
 @author: Ashok Kumar Jha
 """
-import configparser
-import dbconnection
+from configparser import RawConfigParser
+from dbconnection import dbconnection 
 
-config = configparser.RawConfigParser()
+config = RawConfigParser()
 config.read('appconfig.properties')
 
-app_cfg = dict(config.items('APP SECTION'))
-
-conn = dbconnection.getdbconnection(dict(config.items('DB SECTION')))
-print(conn)
-dbconnection.closedbconnection(conn)
+if __name__ == '__main__' :
+    dbc = dbconnection(dict(config.items('DB SECTION'))) 
+    try:
+        print(config)
+        print(dbc)
+        dbc.createconnection()
+        for db in dbc.dbList():
+            print(db)
+        dbc.createdb()
+        dbc.createTable()
+    except Exception as ex:
+        print(ex)
+    finally:
+        dbc.closedbconnection()
--- Practice/RestProject/dbconnection.py
+++ Practice/RestProject/dbconnection.py
@@ -7,14 +7,81 @@ Created on Sat Jun 19 14:32:37 2021
 
 import mysql.connector 
 
-def  getdbconnection(dct):
-    print(dct)
-    return mysql.connector.connect(host= dct['host'],user= dct['key'],password=dct['val'] )
-    #return connect(host= 'localhost',user= 'root',password='Ashu.Kr.Jha@gma1l.c0m' )
- 
-def closedbconnection(conn):
-    if conn and conn.is_connected():
+class dbconnection(object):
+    def __init__(self, db_cfg):
+        '''
+            Create Database communication interface
+            db_cfg: {host:<HOST IP>,key:<Database User ID>,val:<password>,db:<Database Name>}
+        '''        
+        self.db_cfg = db_cfg
+
+    def createconnection(self):
+        '''
+            Return Database connection Based on Configuration
+        '''
+        print(self.db_cfg)
         try:
-            conn.close()
-        except mysql.connector.Error as e:
-            print(e)
+             self.conn = mysql.connector.connect(host= self.db_cfg['host'],
+                                       user= self.db_cfg['key'],
+                                       password=self.db_cfg['val'] )
+        except Exception as ex:
+            raise ex
+
+     
+    def createdb(self):
+        '''
+            Create Database Database based on onfiguration 
+            Raise error in case of failure
+        '''
+        try:
+            with self.conn.cursor() as cursor:
+                cursor.execute("CREATE DATABASE IF NOT EXISTS "+self.db_cfg['db'])
+        except mysql.connector.Error as ex:
+            raise ex
+            
+    def dbList(self):
+        
+        dblst=[]
+        try:
+            show_db_query = "SHOW DATABASES"
+            with self.conn.cursor() as cursor:
+                cursor.execute(show_db_query)
+                for db in cursor:
+                    dblst.append(db)      
+        except mysql.connector.Error as ex:
+            raise ex
+        return dblst
+    
+    def createTable(self):
+        sql = "CREATE TABLE IF NOT EXISTS " +self.db_cfg['db']+'''.Students 
+                    (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255))'''
+        try:
+            with self.conn.cursor() as cursor:
+                cursor.execute(sql) 
+        except mysql.connector.Error as ex:
+            raise ex
+
+    def fetch(self,sql):
+        try:
+            with self.conn.cursor() as cursor:
+                cursor.execute(sql) 
+        except mysql.connector.Error as ex:
+            raise ex  
+
+    def insert(self,insertstmt):
+        try:
+            with self.conn.cursor() as cursor:
+                cursor.execute(insertstmt) 
+        except mysql.connector.Error as ex:
+            raise ex    
+            
+    def closedbconnection(self):
+        '''
+            Close Database connection
+        '''
+        if self.conn.is_connected():
+            try:
+                self.conn.close()
+                self.conn=None
+            except mysql.connector.Error as ex:
+                raise ex
