8287832438e7a7e928c1420664946a63156aaf6b
Ashok Kumar Jha <ashu.kr.jha@gmail.com>
2021-06-28 16:34:40 +0530

    wip

--- Practice/finance/Lesson3.py
+++ Practice/finance/Lesson3.py
@@ -5,67 +5,31 @@ Technical Analysis
 Calculate with Column
  Add Column
  Drop Column
- Min, Max,ArgMin,ArgMax
+ min, max,argmin (Deprecated instead use idxmin),
+  argmax(deprecated instead use idxmax)
  Mean
 @author: Ashok Kumar Jha
 """
 
-import pandas as pd
-import numpy as np
-from matplotlib import pyplot as plt
+from TechAnalysis import TechAnalysis
 
 
-# REading the CSV, Setting Index to First column of CSV, Changeing Data
-# type of Index to DateTime
-data = pd.read_csv("data/AAPL.csv", index_col=0, parse_dates=True)
-
-# Adding Data Column
-data["daily_ch"] = data['Open'] - data['Close']
-
-data["normalize"] = data['Close']/data['Close'].iloc[0]
-data["%-Change"] = data['Close'].pct_change()
-
-data["Log Returns"] = np.log(data['Close']/data['Close'].shift())
-
-print(data.head())
-
-print('Over all Minimum value in every Column')
-print(data.min())
-
+ta = TechAnalysis()
+ta.addDailyChange()
+ta.addpchange()
+ta.addnormalize()
+ta.printminima()
 print('Minimum value in every Column and index')
-print('daily_ch minimum :', data['daily_ch'].min(),
-      ' and index:', data['daily_ch'].idxmin())
-
+print('daily_ch minimum :', ta.data['daily_ch'].min(),
+      ' and index:', ta.data['daily_ch'].min())
 print('Over all Maximum value in every Column')
-print(data.max())
-print('Close Maximum :', data['Close'].max(), ' and index:',
-      data['Close'].idxmax())
-print('Close Maximum :', data['Close'].max(), ' and index:',
-      data['Close'].idxmax())
-
-stddev = data["Log Returns"].std()
-
-volatility = stddev*(252**.5)
-print("Standrad Deviation: {0}  Volaitility: {1}".format(stddev, volatility))
-
-vltstr = str(round(volatility, 4)*100)
-
-fig, ax = plt.subplots()
-
-data["Log Returns"].plot.hist(ax=ax, bins=50, alpha=0.6, color='b')
-ax.set_xlabel('Log Return')
-ax.set_ylabel('Freq of Log return')
-ax.set_title('AAPL volatility: '+vltstr)
-
-# Moving Average 10 day
-data['MVA 10'] = data['Close'].rolling(10).mean()
-
-# Exponential Moving Average 10 day
-data['EMA 10'] = data['Close'].ewm(span=10, adjust=False).mean()
-
-fig, ax = plt.subplots()
-# data[['MVA 10','EMA 10']].plot(ax=ax)
-print('Lesser Data')
-data[['MVA 10', 'EMA 10']].loc['2020-12-01':].plot(ax=ax)
-ax.set_title("Actual, Moving Average and Exponential Moving Average")
-data[['Close']].loc['2020-12-01':].plot(ax=ax,alpha=0.25)
+print(ta.data.max())
+print('Close Minimum :', ta.data['Close'].min(), ' and index:',
+      ta.data['Close'].argmin())
+print('Close Maximum :', ta.data['Close'].max(), ' and index:',
+      ta.data['Close'].argmax())
+ta.drawvolatilitygraph()
+ta.drawavggraph()
+ta.drawavggraphL(ils=60)
+ta.drawactavggraphL()
+print(ta.data.head())
--- Practice/finance/test.py
+++ Practice/finance/test.py
@@ -5,25 +5,21 @@ Created on Sat Jun 26 20:01:30 2021
 @author: Ashok Kumar Jha
 """
 
-# Yahoo recently has become an unstable data source.
-# If it gives an error, you may run the cell again, or try yfinance
-#import pandas as pd
 
 from pandas_datareader import data as pdr
 
 import datetime as dt
 
 
-start_date = dt.datetime(1990,1,1)
-end_date = dt.datetime(2001,3,31)
-# Set the ticker
-ticker = 'AMZN'
+def getYFData(cfg):
+    '''
+       Get Yahoo Finance Data
+    '''
+    ticker = cfg['ticker']
+    start_date = dt.strptime(cfg['SD'], cfg["DATE-FORMAT"])
+    end_date = dt.strptime(cfg['ED'], cfg["DATE-FORMAT"])
+    return pdr.get_data_yahoo(ticker, start_date, end_date)
 
-print(type(pdr))
-# Get the data
-data = pdr.get_data_yahoo(ticker, start_date, end_date)
-print(type(data.t))
-print(data.head())
 
 
 
