d115e262536c4ad268cdac9b25493e710e56a465
Ashok Kumar Jha <ashu.kr.jha@gmail.com>
2021-06-30 18:01:28 +0530

    Update q6a.py

--- Student/X11/Assignment/ExAM/q6a.py
+++ Student/X11/Assignment/ExAM/q6a.py
@@ -1,6 +1,6 @@
 import re
 x= input("Please enter a stringh:")
-x=x.replace(" ","")
+x=x.replace(" ", "")
 pattern='[0-9]'
-x = re.sub(pattern, '#', i) for i in x
+x = "".join([re.sub(pattern, '#', i) for i in x])
 print(x)
